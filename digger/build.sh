89150c75e522348d1ce4e477e6f9ac45896b343f
xafero <gberendt@outlook.com>
2021-07-06 15:10:27 +0200

    Updating package json

--- digger/build.sh
+++ digger/build.sh
@@ -3,4 +3,8 @@
 # npx tsc
 # npx webpack -w
 
-npm run build
+rm obj/*
+npm run asbuild
+echo -n "function forceImportWasm() { self.getMainWasm = function() { return \"" > dist/binrepo.js
+base64 obj/main.wasm --wrap=0 >> dist/binrepo.js
+echo "\"; } }" >> dist/binrepo.js
